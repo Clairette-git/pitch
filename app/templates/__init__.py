573b135d84ff8a45b2f7bf02cdd1c52f9467e225
Clairette-git <marcletty12@gmail.com>
2021-04-05 21:54:51 +0200

    adding email file in the template

--- app/templates/__init__.py
+++ app/templates/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import config_options
+from flask.config import config_options
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
